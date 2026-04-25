cca89d9ad9b55064804c7c8071218c010807a7ac
RELSA <Hayarow.il@gmail.com>
2026-04-25 21:58:13 +0400

    исправления для cloudflare N1

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEB is not set")
+    raise ValueError("BOT_TOKEN is not set")
 
 DB_URL = os.getenv("DB_URL")
 if not DB_URL:
-    raise ValueError("DataBase URL is not set")
+    raise ValueError("DB_URL is not set")
 
-ADMIN_ID = [int(id_str) for id_str in os.getenv("ADMIN_ID", "").split(",") if id_str]
+ADMIN_ID = [
+    int(id_str.strip()) 
+    for id_str in os.getenv("ADMIN_ID", "").split(",") 
+    if id_str.strip()
+]
